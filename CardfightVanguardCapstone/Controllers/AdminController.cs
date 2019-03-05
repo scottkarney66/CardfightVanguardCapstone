9de1ec4398f34dd6a220b56086fd0f52f299d4f7
scottkarney66 <47334404+scottkarney66@users.noreply.github.com>
2019-03-05 16:41:59 -0600

    Add files via upload

---
+++ CardfightVanguardCapstone/Controllers/AdminController.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CardfightVanguardCapstone.Common;
+using CardfightVanguardCapstone.Models;
+using DataAccessLayer;
+using DataAccessLayer.DataClasses;
+
+namespace CardfightVanguardCapstone.Controllers
+{
+    public class AdminController : Controller
+    {
+        public DataAccess da { get; set; }
+        // GET: Adimin view of the users table
+        [HttpGet]
+        public ActionResult SelectUsers()
+        {
+            List<Users> model = new List<Users>();
+            da = new DataAccess();
+            try
+            {
+                
+                int userID = 0;
+                model = Mapper.Map(da.getUserall(userID));
+
+                if (ModelState.IsValid)
+                {
+                    return View(model);
+                }
+                else
+                {
+                    return View();
+                }
+                
+
+            }
+            catch
+            {
+
+            }
+            return View();
+        } 
+        
+        [HttpGet]
+        public ActionResult  Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Users model)
+        {
+            ActionResult oResponse = null;
+
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return View(model);
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch (Exception error)
+            {
+
+                da.LogError(error);
+                return View();
+            }
+            finally
+            {
+                oResponse = RedirectToAction("LoginOut", "Admin");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DeactivateUsers(int UsersToDeactivate)
+        {
+            List<Users> model = new List<Users>();
+            return View("Users", model);
+        }
+        [HttpGet]
+        public ActionResult EditUsers()
+        {
+            List<Users> model = new List<Users>();
+            return View("Users", model);
+
+        }
+
+        [HttpGet]
+        public ActionResult LogInOut()
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return View();
+                }
+                else
+                {
+                    return View();
+                }
+
+            }
+            catch (Exception error)
+            {
+
+                da.LogError(error);
+                return View();
+
+            }
+        }
+        [HttpPost]
+        public ActionResult LogInOut(Users model)
+        {
+            
+            ActionResult oResponse = null;                                   
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                   // UserDO 
+                    
+                    if (//user.Username != null )
+                    {
+                        //Session["Username"] = user.Username;
+                        //Session["Password"] = user.Password;
+                        //Session.Timeout = 10;
+
+
+                        oResponse = RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        oResponse = RedirectToAction("Register", "Admin");
+                    }
+                    
+                }
+                else
+                {
+                    oResponse = RedirectToAction("Register", "Admin");
+                }
+            }
+            catch (Exception error)
+            {
+                da.LogError(error);
+                return View();
+            }
+            finally
+            {
+                
+            }
+            return oResponse;
+        }
+
+        [HttpGet]
+        public ActionResult EditUsers(int UserstoEdit)
+        {
+            List<Users> model = new List<Users>();
+            return View("Users", model);
+        }
+
+    
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ CardfightVanguardCapstone/Controllers/TestController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using BuisnessLayer;
+using CardfightVanguardCapstone.Common;
+using CardfightVanguardCapstone.Models;
+using DataAccessLayer;
+
+namespace CardfightVanguardCapstone.Controllers
+{
+
+    public class TestController : Controller
+    {
+        public DataAccess da { get; set; }
+        // GET: Test
+        public ActionResult DataTableExample()
+        {
+            return View();
+        }
+
+        public ActionResult MessageExample()
+        {
+
+            Message m = new Message();
+            m.State = "error";
+            if (ModelState.IsValid)
+            {
+                if (m.State == "success")
+                {
+                    //do something
+                    m.Description = "Success message text";
+                    return View(m);
+                }
+                else
+                {
+                    m.Description = "Error occurred!!!!!!!";
+                    return View(m);
+                }
+
+
+            }
+
+            else
+            {
+                return View();
+            }
+            //return View();
+        }
+
+        public ActionResult SelectUsers()
+        {
+            List<Users> model = new List<Users>();
+            //Users u = new Users();
+            da = new DataAccess();
+            int userID = 0;
+            try
+            {
+                //testing only
+                
+                model = Mapper.Map(da.getUserall(userID));
+                if (ModelState.IsValid)
+                {
+
+
+                    return View(model);
+
+
+                }
+                else
+                {
+                    return View();
+                }
+
+            }
+            catch (Exception error)
+            {
+
+                da.LogError(error);
+                return View();
+
+            }
+
+
+
+
+
+
+        }
+        
+    }
+}
