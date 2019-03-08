e34233068bf6b8c557b830d66e74b96130cd0670
scottkarney66 <47334404+scottkarney66@users.noreply.github.com>
2019-03-08 13:52:58 -0600

    Add files via upload

---
+++ CardfightVanguardCapstone/Views/CFV/Vanguard_Cards.cs
@@ -0,0 +1,26 @@
+﻿namespace CardfightVanguardCapstone.Views.CFV
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class Vanguard_Cards
+    {
+        public int CardID { get; set; }
+        public string CardName { get; set; }
+        public string CardEffect { get; set; }
+        public string FlavorText { get; set; }
+        public string Clan { get; set; }
+        public string Power { get; set; }
+        public string Race { get; set; }
+        public string Grade { get; set; }
+        public string Sheild { get; set; }
+        public string Critical { get; set; }
+        public string Trigger { get; set; }
+        public string Skill { get; set; }
+        public string CardType { get; set; }
+        public string Nation { get; set; }
+        public int DeckID { get; set; }
+    }
+}
---
+++ CardfightVanguardCapstone/Views/CFV/Vanguard_Decks.cs
@@ -0,0 +1,13 @@
+﻿namespace CardfightVanguardCapstone.Views.CFV
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class Vanguard_Decks
+    {
+        public int DeckID { get; set; }
+        public string DeckName { get; set; }
+    }
+}
